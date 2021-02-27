2812536e1cdeb87c6514d83f322aa8bfcb83c911
Murkz <67777430+Murkz21@users.noreply.github.com>
2021-02-27 17:55:35 +0800

    Update Why You Don't Wanna Mass Up with Generators and Corutines in One Function.py
    Caution: this will blow your mind!!!

--- Why You Don't Wanna Mass Up with Generators and Corutines in One Function.py
+++ Why You Don't Wanna Mass Up with Generators and Corutines in One Function.py
@@ -15,7 +15,7 @@ for num in gen_num(): # The for-loop will next()/send(None) to the coroutine but
         g.__next__() # Every __next__()/send(None) and send() will increase Coroutine Yield Number by 1
         next(g) # Same as g.__next__() and send(None)
         print(f'Coroutine Yield Number: {send_value}')
-        send_value = g.send(1000*num) # Only trigger the Coroutine Yield Number increasing here all at once...
+        send_value = g.send(1000*num) # Send back the yield number of the coroutine to 'send_value'.
         print(f'Coroutine Yield Number: {send_value}')
         print(f'For Loop Yield Number: {num}')
     except StopIteration:
